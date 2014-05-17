180dfd16850596c06ecdf7b4f64749445f76a1e2
Daniel Ayala <dmayala@csupomona.edu>
2014-05-17 09:38:13 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,5 +38,19 @@ describe "Static pages" do
       end
     end
   end
+  describe "Static pages" do
+    describe "Contact page" do
+      
+      it "should have the content 'Contact'" do
+        visit '/static_pages/contact'
+        expect(page).to have_content('Contact')
+      end
+
+      it "should have the right title" do
+        visit '/static_pages/contact'
+        expect(page) .to have_title("Contact")
+      end
+    end
+  end
 end
 
