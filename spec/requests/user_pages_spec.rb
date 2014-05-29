a2789141a47104ce6af9f368fe02054de71be453
Daniel Ayala <dmayala@csupomona.edu>
2014-05-28 21:27:12 -0700

    Add microposts to users show

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe User do
+describe "User pages" do
 
   before do
    @user = User.new(name: "Example User", email: "user@example.com",
@@ -127,5 +127,22 @@ describe User do
     before { @user.save }
 
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
   end
 end
